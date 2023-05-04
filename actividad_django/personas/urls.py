3d45eeb5ec34dac0c1d93fc6a4fb4effe4e1e0f5
Simonky <simonasb19@gmail.com>
2023-05-04 17:32:50 -0400

    Funcionalidad de eliminar

--- actividad_django/personas/urls.py
+++ actividad_django/personas/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import listado_personas
+from .views import listado_personas, eliminar_personas
 urlpatterns = [
-    path("", listado_personas, name="listado_personas")
+    path("", listado_personas, name="listado_personas"),
+    path("<int:pk>/eliminar", eliminar_personas, name="eliminar_persona")
 ]
--- actividad_django/personas/views.py
+++ actividad_django/personas/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Persona
 # Create your views here.
 def listado_personas(request):
     personas = Persona.objects.all()
     return render(request, 'personas/listado_personas.html', {'personas': personas})
+
+
+def eliminar_personas(request, pk):
+    persona = get_object_or_404(Persona, pk=pk)
+    if request.method == 'POST':
+        persona.delete()
+        return redirect('listado_personas')
